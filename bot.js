cc5d52a89db5f32d6bcde5423b248dd7a96046fb
faresmnasri <31959540+faresmnasri@users.noreply.github.com>
2018-12-17 18:17:15 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,44 +1,6 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const moment = require("moment");  
-const fs = require("fs"); 
 const Eris = require("eris");
 var bot = new Eris(process.env.BOT_TOKEN);
-
-
-
-
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`-setstats`,"http://twitch.tv/S-F")
-    console.log('')
-    console.log('')
-    console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-    console.log(`[Start] ${new Date()}`);
-    console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-    console.log('')
-    console.log('╔[════════════════════════════════════]╗');
-    console.log(`Logged in as * [ " ${client.user.username} " ]`);
-    console.log('')
-    console.log('Informations :')
-    console.log('')
-    console.log(`servers! [ " ${client.guilds.size} " ]`);
-    console.log(`Users! [ " ${client.users.size} " ]`);
-    console.log(`channels! [ " ${client.channels.size} " ]`);
-    console.log('╚[════════════════════════════════════]╝')
-    console.log('')
-    console.log('╔[════════════]╗')
-    console.log(' Bot Is Online')
-    console.log('╚[════════════]╝')
-    console.log('')
-    console.log('')
-});
-
-
-
-const Eris = require("eris");
-var bot = new Eris(process.env.BOT_TOKENs);
-var id = "519106470042927116";
+var id = "523649200852959233";
 //** لاتعدل على شي تحت ذا **//
 bot.on("ready", () => {
 onstart();
@@ -65,10 +27,9 @@ if (hours == 0) {
 hours = 24;
 }
 
-bot.editChannel(id, { name : "●⌠  " + hours + ":" + minutes + "  ⌡●"});
+bot.editChannel(id, { name : "●⌠ " + hours + ":" + minutes + " ⌡●"});
 }, 60000);
 
 }
 
 bot.connect();
-
