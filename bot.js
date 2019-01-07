8e07fa7374def490e07f06e294f2a52ed6320a72
faresmnasri <31959540+faresmnasri@users.noreply.github.com>
2019-01-07 19:05:04 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Eris = require("eris");
 var bot = new Eris(process.env.BOT_TOKEN);
-var id = "523649200852959233";
+var id = "529084682567024650";
 //** لاتعدل على شي تحت ذا **//
 bot.on("ready", () => {
 onstart();
@@ -9,27 +9,27 @@ onstart();
 function onstart(){
 
 setInterval(function(){
-
+    
 var currentTime = new Date(),
 hours = currentTime.getHours() + 1 ,
 minutes = currentTime.getMinutes(),
 seconds = currentTime.getSeconds();
-
+    
 if (minutes < 10) {
 minutes = "0" + minutes;
 }
-var suffix = "صباحاَ";
+
 if (hours >= 24) {
-suffix = "مساء";
+
 hours = hours - 24;
 }
-if (hours == 0) {
+if (hours == 24) {
 hours = 24;
 }
-
-bot.editChannel(id, { name : "●⌠ " + hours + ":" + minutes + " ⌡●"});
+    
+bot.editChannel(id, { name :"   ●⌠" + hours +  ":"  + minutes + "⌡●   "});
 }, 60000);
-
+    
 }
-
+    
 bot.connect();
